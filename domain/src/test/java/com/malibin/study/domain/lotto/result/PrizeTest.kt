91dcd15daefb6203432c9b41781b639abbcaae6f
ChunBaee <mljung10@naver.com>
2023-02-04 17:42:23 +0900

    fix: 스텝2 3차 TC 수정

--- domain/src/test/java/com/malibin/study/domain/lotto/result/PrizeTest.kt
+++ domain/src/test/java/com/malibin/study/domain/lotto/result/PrizeTest.kt
@@ -16,8 +16,8 @@ class PrizeTest {
                         "0,false,Lose"])
     @ParameterizedTest
     fun `로또의 결과값을 잘 반환할 수 있다`(count: Int, has: Boolean, prize: Prize) {
-        val assertValue = Prize.find(count, has).name
-        assertThat(assertValue).isEqualTo(prize.name)
+        val assertValue = Prize.find(count, has)
+        assertThat(assertValue).isEqualTo(prize)
     }
 
     @CsvSource(value = ["6,true,6,false",
@@ -28,8 +28,8 @@ class PrizeTest {
                         "0,true,0,false"])
     @ParameterizedTest
     fun `보너스 번호가 상관 없는 경우를 잘 판단할 수 있다`(count: Int, has: Boolean, count2 : Int, has2 : Boolean) {
-        val bonusValue = Prize.find(count, has).name
-        val nonBonusValue = Prize.find(count2, has2).name
+        val bonusValue = Prize.find(count, has)
+        val nonBonusValue = Prize.find(count2, has2)
         assertThat(bonusValue).isEqualTo(nonBonusValue)
     }
 
--- domain/src/test/java/com/malibin/study/domain/lotto/ticket/WinningTicketTest.kt
+++ domain/src/test/java/com/malibin/study/domain/lotto/ticket/WinningTicketTest.kt
@@ -8,21 +8,21 @@ import org.junit.jupiter.api.Test
 class WinningTicketTest {
     @Test
     fun `보너스 번호와 당첨 번호는 중복 될 수 없다`() {
-        val targetNumbers = LottoTicket(*intArrayOf(1, 2, 3, 4, 5, 6))
+        val targetNumbers = LottoTicket(1, 2, 3, 4, 5, 6)
         val bonusNumber = LottoNumber.of(5)
 
         val assertException = runCatching { WinningTicket(targetNumbers, bonusNumber) }.exceptionOrNull()
 
         assertThat(assertException)
             .hasMessageThat()
-            .contains("보너스 번호 ($bonusNumber)는 당첨번호(${targetNumbers.lottoNumbers.joinToString(", ")})와 중복될 수 없습니다.")
+            .contains("보너스 번호 (5)는 당첨번호(1, 2, 3, 4, 5, 6)와 중복될 수 없습니다.")
     }
 
     @Test
     fun `한 개의 로또 티켓의 당첨 등수를 잘 반환한다`() {
-        val winningTicket = LottoTicket(*intArrayOf(1, 2, 3, 4, 5, 6))
+        val winningTicket = LottoTicket(1, 2, 3, 4, 5, 6)
         val bonus = LottoNumber.of(7)
-        val otherTicket = LottoTicket(*intArrayOf(1, 2, 3, 4, 5, 6))
+        val otherTicket = LottoTicket(1, 2, 3, 4, 5, 6)
 
         val assertValue = WinningTicket(winningTicket, bonus).compareWith(otherTicket)
 
@@ -31,22 +31,22 @@ class WinningTicketTest {
 
     @Test
     fun `여러 개의 로또 티켓들의 당첨 등수와 당첨된 티켓의 갯수를 잘 반환한다`() {
-        val winningTicket = LottoTicket(*intArrayOf(1, 2, 3, 4, 5, 6))
+        val winningTicket = LottoTicket(1, 2, 3, 4, 5, 6)
         val bonus = LottoNumber.of(7)
 
-        val ticket1 = LottoTicket(*intArrayOf(1, 2, 3, 8, 9, 10))
-        val ticket2 = LottoTicket(*intArrayOf(1, 2, 3, 4, 5, 6))
-        val ticket3 = LottoTicket(*intArrayOf(1, 2, 3, 4, 5, 7))
+        val ticket1 = LottoTicket(1, 2, 3, 8, 9, 10)
+        val ticket2 = LottoTicket(1, 2, 3, 4, 5, 6)
+        val ticket3 = LottoTicket(1, 2, 3, 4, 5, 7)
         val comparedTickets = listOf(ticket1, ticket2, ticket3)
 
         val assertValue = WinningTicket(winningTicket, bonus).compareWith(comparedTickets)
 
         val expectValue = mapOf<Prize, Int>(
-            Pair(Prize.Fifth, 1),
-            Pair(Prize.First, 1),
-            Pair(Prize.Second, 1)
+            Prize.Fifth to 1,
+            Prize.First to 1,
+            Prize.Second to 1
         )
 
-        assertThat(assertValue.toString()).isEqualTo(expectValue.toString())
+        assertThat(assertValue).isEqualTo(expectValue)
     }
 }
